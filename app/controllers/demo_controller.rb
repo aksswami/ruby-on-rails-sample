91d3fe23cde38eb51a19979dd2b39e3ec99a9751
Amit Kumar Swami <amitiitm2009@gmail.com>
2015-04-04 16:41:06 +0530

    First Commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,21 @@
+class DemoController < ApplicationController
+
+	layout false
+
+  	def index
+ 	end
+
+ 	def hello
+ 		#render('index')
+ 		@array = [1,2,3,4]
+ 		@id = params['id'].to_i
+ 		@page = params[:page].to_i
+ 		puts @id 
+ 		puts @page
+ 	end
+
+ 	def other_hello
+ 		redirect_to(:controller => 'demo', :action => 'index')
+ 	end
+
+end
